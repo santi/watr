bcc67b480a6309f417d9910bfde7cbd1cd07349f
Vemund Santi <vemund@santi.no>
2019-11-12 19:43:39 +0100

    feat: remove unused variable

--- src/installSW.js
+++ src/installSW.js
@@ -2,10 +2,12 @@ export const install = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', async function() {
       const registrations = await navigator.serviceWorker.getRegistrations()
-      for(let registration of registrations) {
-        registration.unregister();
+      registrations.forEach(registration => {
+        registration.unregister()
         console.log("uninstalled old service worker")
-      }
+      })
+        
+      
       navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
