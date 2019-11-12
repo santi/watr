4e606fbedd1ec24c0f47a8c8306191d00b7bc6fb
Jostein Kringlen <jostein.kringlen@finn.no>
2019-11-12 18:39:20 +0100

    add routing + change folder structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import Main from "./routes/Main";
+import Settings from "./routes/Settings";
+import NotFound from "./routes/notfound";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/settings" component={Settings} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-Notification.requestPermission() 
+Notification.requestPermission();
 serviceWorker.register();
